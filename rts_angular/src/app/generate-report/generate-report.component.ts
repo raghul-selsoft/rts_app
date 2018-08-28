9563a0ed297b2d42245f3fb6ec91d7959534b4be
rathina-selsoft <rathina@selsoft.com>
2018-08-28 13:27:10 +0530

    report filter added

--- rts_angular/src/app/generate-report/generate-report.component.ts
+++ rts_angular/src/app/generate-report/generate-report.component.ts
@@ -4,6 +4,7 @@ import { RequirementsService } from '../Services/requirements.service';
 import { HideComponentService } from '../Services/hide-component.service';
 import { SubmissionService } from '../Services/submission.service';
 import * as moment from 'moment';
+import * as _ from 'underscore';
 import { ApiUrl } from '../Services/api-url';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
@@ -22,12 +23,20 @@ export class GenerateReportComponent implements OnInit {
   private approvedsubmissions: any;
   private rtsCompanyId: any;
   private currentDate: Date;
-  private approvedsubmissionsLength: any;
+  private approvedSubmissionsLength: any;
   private userRole: any;
   private baseUrl: any;
   public myForm: FormGroup;
   private startDate: any;
   private toDate: any;
+  private isTeam: boolean;
+  private isClient: boolean;
+  private isRecruiter: boolean;
+  private filter: string;
+  private clients: any;
+  private teams: any;
+  private teamUsers: any;
+  private selectedSubmissions: any;
 
   constructor(private loggedUser: LoggedUserService,
     private requirementService: RequirementsService,
@@ -41,6 +50,7 @@ export class GenerateReportComponent implements OnInit {
     this.userRole = this.rtsUser.role;
     this.rtsUserId = this.rtsUser.userId;
     this.currentDate = new Date();
+    this.filter = '';
   }
 
   ngOnInit() {
@@ -48,9 +58,27 @@ export class GenerateReportComponent implements OnInit {
       fromDate: [''],
       toDate: ['']
     });
-    this.toDate = moment(this.currentDate).format('YYYY-MM-DD');
+    const currentDateMoment: moment.Moment = moment(this.currentDate);
+    this.startDate = currentDateMoment.subtract(3, 'days').format('YYYY-MM-DD');
+    this.getCommonDetails();
     this.getApprovedSubmissions();
     this.baseUrl = ApiUrl.BaseUrl;
+  }
+
+  getCommonDetails() {
+    const companyId = {
+      userId: this.rtsUserId
+    };
+
+    this.requirementService.commonDetails(companyId)
+      .subscribe(
+        data => {
+          if (data.success) {
+            this.clients = data.clients;
+            this.teams = data.teams;
+            this.teamUsers = data.myTeamUser;
+          }
+        });
   }
 
   filterByDate(form: FormGroup) {
@@ -60,41 +88,81 @@ export class GenerateReportComponent implements OnInit {
     } else {
       this.startDate = '';
     }
+    this.getApprovedSubmissions();
 
 
-    console.log(this.startDate, this.toDate);
+  }
 
+  filterBy(value) {
+
+    if (value === 'team') {
+      this.isTeam = true;
+      this.isClient = false;
+      this.isRecruiter = false;
+    } else if (value === 'client') {
+      this.isClient = true;
+      this.isTeam = false;
+      this.isRecruiter = false;
+    } else if (value === 'recruiter') {
+      this.isRecruiter = true;
+      this.isTeam = false;
+      this.isClient = false;
+    } else if (value === '') {
+      this.filter = '';
+      this.isRecruiter = false;
+      this.isTeam = false;
+      this.isClient = false;
+    }
+  }
+
+  selectTeam(event) {
+    if (event === 'selectAll') {
+      this.selectedSubmissions = this.approvedsubmissions;
+      this.selectedSubmissionDetails(this.selectedSubmissions);
+    } else {
+      this.selectedSubmissions = [];
+      this.selectedSubmissions = _.where(this.approvedsubmissions, { teamId: event });
+      this.selectedSubmissionDetails(this.selectedSubmissions);
+    }
+  }
+
+  selectClient(event) {
+    if (event === 'selectAll') {
+      this.selectedSubmissions = this.approvedsubmissions;
+      this.selectedSubmissionDetails(this.selectedSubmissions);
+    } else {
+      this.selectedSubmissions = [];
+      this.selectedSubmissions = _.where(this.approvedsubmissions, { clientId: event });
+      this.selectedSubmissionDetails(this.selectedSubmissions);
+    }
+  }
+
+  selectRecruiter(event) {
+    if (event === 'selectAll') {
+      this.selectedSubmissions = this.approvedsubmissions;
+      this.selectedSubmissionDetails(this.selectedSubmissions);
+    } else {
+      this.selectedSubmissions = [];
+      this.selectedSubmissions = _.where(this.approvedsubmissions, { recruiterId: event });
+      this.selectedSubmissionDetails(this.selectedSubmissions);
+    }
   }
 
   getApprovedSubmissions() {
 
+    this.toDate = moment(this.currentDate).format('YYYY-MM-DD');
+
     const userId = {
       userId: this.rtsUserId,
+      fromDate: this.startDate,
+      toDate: this.toDate
     };
 
     this.submissonService.approvedSubmissionDetails(userId)
       .subscribe(
         data => {
           if (data.success) {
-            this.approvedsubmissions = data.submissionReport;
-            this.approvedsubmissionsLength = this.approvedsubmissions.length;
-            for (const submission of this.approvedsubmissions) {
-              const diff = Math.floor(this.currentDate.getTime() - submission.submissionDate);
-              const day = 1000 * 60 * 60 * 24;
-              const days = Math.floor(diff / day);
-              const weeks = Math.floor(days / 7);
-              const months = Math.floor(days / 31);
-              const years = Math.floor(months / 12);
-              if (days < 7) {
-                submission.age = days + ' days ago';
-              } else if (weeks < 4) {
-                submission.age = weeks + ' weeks ago';
-              } else if (months < 12) {
-                submission.age = months + ' months ago';
-              } else {
-                submission.age = years + ' years ago';
-              }
-            }
+            this.submissionDetails(data);
           }
         });
   }
@@ -114,5 +182,51 @@ export class GenerateReportComponent implements OnInit {
         });
 
   }
+
+  submissionDetails(data) {
+    this.approvedsubmissions = data.submissionReport;
+    this.selectedSubmissions = this.approvedsubmissions;
+    this.approvedSubmissionsLength = this.selectedSubmissions.length;
+    for (const submission of this.approvedsubmissions) {
+      const diff = Math.floor(this.currentDate.getTime() - submission.submissionDate);
+      const day = 1000 * 60 * 60 * 24;
+      const days = Math.floor(diff / day);
+      const weeks = Math.floor(days / 7);
+      const months = Math.floor(days / 31);
+      const years = Math.floor(months / 12);
+      if (days < 7) {
+        submission.age = days + ' days ago';
+      } else if (weeks < 4) {
+        submission.age = weeks + ' weeks ago';
+      } else if (months < 12) {
+        submission.age = months + ' months ago';
+      } else {
+        submission.age = years + ' years ago';
+      }
+    }
+  }
+
+  selectedSubmissionDetails(data) {
+    this.selectedSubmissions = data;
+    this.approvedSubmissionsLength = this.selectedSubmissions.length;
+    for (const submission of this.selectedSubmissions) {
+      const diff = Math.floor(this.currentDate.getTime() - submission.submissionDate);
+      const day = 1000 * 60 * 60 * 24;
+      const days = Math.floor(diff / day);
+      const weeks = Math.floor(days / 7);
+      const months = Math.floor(days / 31);
+      const years = Math.floor(months / 12);
+      if (days < 7) {
+        submission.age = days + ' days ago';
+      } else if (weeks < 4) {
+        submission.age = weeks + ' weeks ago';
+      } else if (months < 12) {
+        submission.age = months + ' months ago';
+      } else {
+        submission.age = years + ' years ago';
+      }
+    }
+  }
 }
+
 
