d0f94825e9cec07021e761dd5b3b9dcabfa6a734
rathina-selsoft <rathina@selsoft.com>
2018-09-05 19:10:02 +0530

    dashboard submissions move to submission component

--- rts_angular/src/app/client-requirements/client-requirements.component.ts
+++ rts_angular/src/app/client-requirements/client-requirements.component.ts
@@ -20,6 +20,7 @@ export class ClientRequirementsComponent implements OnInit {
   private selectedRequirements: any;
   private fromDate: any;
   private toDate: any;
+  private currentDate: Date;
 
   constructor(
     private loggedUser: LoggedUserService,
@@ -28,6 +29,7 @@ export class ClientRequirementsComponent implements OnInit {
   ) {
     this.rtsUser = JSON.parse(this.loggedUser.loggedUser);
     this.rtsUserId = this.rtsUser.userId;
+    this.currentDate = new Date(Date.now());
   }
 
   ngOnInit() {
@@ -54,7 +56,23 @@ export class ClientRequirementsComponent implements OnInit {
           if (data.success) {
             this.selectedRequirements = data.requirements;
             this.requirementsLength = this.selectedRequirements.length;
-
+            for (const require of this.selectedRequirements) {
+              const diff = Math.floor(this.currentDate.getTime() - require.createdOn);
+              const day = 1000 * 60 * 60 * 24;
+              const days = Math.floor(diff / day);
+              const weeks = Math.floor(days / 7);
+              const months = Math.floor(days / 31);
+              const years = Math.floor(months / 12);
+              if (days < 7) {
+                require.age = days + ' days ago';
+              } else if (weeks < 4) {
+                require.age = weeks + ' weeks ago';
+              } else if (months < 12) {
+                require.age = months + ' months ago';
+              } else {
+                require.age = years + ' years ago';
+              }
+            }
           }
         });
   }
