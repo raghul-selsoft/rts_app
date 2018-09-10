88f03f2606fa4e2097df9563ccbfe32fe3b5c94b
rathina-selsoft <rathina@selsoft.com>
2018-09-10 15:33:45 +0530

    dashboard interview report

--- rts_angular/src/app/Services/graph.service.ts
+++ rts_angular/src/app/Services/graph.service.ts
@@ -73,4 +73,24 @@ export class GraphService {
         return '{}';
       });
   }
+
+  getInterviewDetails(graph) {
+    const token = localStorage.getItem('id_token');
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Authorization', token);
+
+    return this.http.post(ApiUrl.BaseUrl + ApiUrl.InterViewReport, graph,
+      { headers: headers })
+      .map(res => {
+        const responseToken = res.headers.get('refresh-token');
+        localStorage.setItem('id_token', responseToken);
+        return res.json();
+      }).catch(err => {
+        if (err.status === 401) {
+          this.loginService.logout();
+        }
+        return '{}';
+      });
+  }
 }
