7cf2338205d187621746b029a84cc40f7ced020a
rathina-selsoft <rathina@selsoft.com>
2018-12-10 18:19:38 +0530

    manage users acending order

--- rts_angular/src/app/manage-users/manage-users.component.ts
+++ rts_angular/src/app/manage-users/manage-users.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { UserService } from '../Services/user.service';
 import { NgProgress } from 'ngx-progressbar';
 import { GraphExpansationComponent } from '../graph-expansation/graph-expansation.component';
+import { Sort } from '@angular/material';
 
 @Component({
   selector: 'app-manage-users',
@@ -19,6 +20,7 @@ export class ManageUsersComponent implements OnInit {
   private userLength: any;
   private rtsCompanyId: any;
   private userRole: any;
+  sortedData: any;
 
   constructor(
     private loggedUser: LoggedUserService,
@@ -71,9 +73,34 @@ export class ManageUsersComponent implements OnInit {
             this.ngProgress.done();
             this.userDetails = data.users;
             this.userLength = this.userDetails.length;
+            this.sortedData = this.userDetails;
           }
         });
 
+  }
+
+  sortData(sort: Sort) {
+    const data = this.userDetails.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'firstName': return this.compare(a.firstName, b.posifirstNametionName, isAsc);
+        case 'lastName': return this.compare(a.lastName, b.lastName, isAsc);
+        case 'email': return this.compare(a.email, b.email, isAsc);
+        case 'role': return this.compare(a.role, b.role, isAsc);
+        case 'status': return this.compare(a.userStatus, b.userStatus, isAsc);
+        default: return 0;
+      }
+    });
+  }
+
+  compare(a, b, isAsc) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
 
 }
