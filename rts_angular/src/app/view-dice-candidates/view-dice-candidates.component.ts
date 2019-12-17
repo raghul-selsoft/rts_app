39949a1b553b44cdf35533d3d905dc1535752496
raghul-selsoft <raghul@selsoftinc.com>
2019-12-17 18:42:18 +0530

    dice canddidates filter by user

--- rts_angular/src/app/view-dice-candidates/view-dice-candidates.component.ts
+++ rts_angular/src/app/view-dice-candidates/view-dice-candidates.component.ts
@@ -5,6 +5,8 @@ import { ToastrService } from 'ngx-toastr';
 import { DiceService } from '../Services/dice.service';
 import * as moment from 'moment';
 import { PageEvent } from '@angular/material';
+import * as _ from 'underscore';
+import { UserService } from '../Services/user.service';
 
 @Component({
   selector: 'app-view-dice-candidates',
@@ -25,12 +27,16 @@ export class ViewDiceCandidatesComponent implements OnInit {
   // pageSize: number;
   lowValue: number = 0;
   highValue: number = 10;
+  selectedCandidates: unknown[];
+  userDetails: any;
+  selectedUser: any;
 
   constructor(
     private loggedUser: LoggedUserService,
     private ngProgress: NgProgress,
     private toastr: ToastrService,
     private diceService: DiceService,
+    private userService: UserService,
   ) {
     this.rtsUser = JSON.parse(this.loggedUser.loggedUser);
     this.rtsUserId = this.rtsUser.userId;
@@ -38,9 +44,26 @@ export class ViewDiceCandidatesComponent implements OnInit {
     this.startDate = new Date(Date.now())
     this.endDate = new Date(Date.now())
     this.currentDate = new Date(Date.now())
+    this.selectedUser = this.rtsUserId;
   }
 
   ngOnInit() {
+    this.getActiveUser();
+  }
+
+  getActiveUser() {
+    const userId = {
+      userId: this.rtsUserId
+    };
+
+    this.userService.getActiveUsers(userId)
+      .subscribe(
+        data => {
+          this.ngProgress.done();
+          if (data.success) {
+            this.userDetails = data.users;
+          }
+        });
   }
 
   public getPaginatorData(event: PageEvent): PageEvent {
@@ -51,6 +74,7 @@ export class ViewDiceCandidatesComponent implements OnInit {
 
   dateFilter() {
     this.ngProgress.start();
+    this.selectedUser = this.rtsUserId;
     const fromDate = moment(this.startDate).format('YYYY-MM-DD');
     const toDate = moment(this.endDate).format('YYYY-MM-DD');
     const submit = {
@@ -65,9 +89,20 @@ export class ViewDiceCandidatesComponent implements OnInit {
           this.ngProgress.done();
           if (data.success) {
             this.candidates = data.diceCandidates;
-            this.candidatesLength = this.candidates.length;
+            for (const candidate of this.candidates) {
+              candidate.userId = candidate.enteredUser.userId;
+            }
+            this.selectedCandidates = this.candidates;
+            this.candidatesLength = this.selectedCandidates.length;
           }
         });
+  }
+
+  filterByUser() {
+    if (this.selectedUser !== undefined) {
+      this.selectedCandidates = _.where(this.candidates, { userId: this.selectedUser });
+      this.candidatesLength = this.selectedCandidates.length;
+    }
   }
 
 }
