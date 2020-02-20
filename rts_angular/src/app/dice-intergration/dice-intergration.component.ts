d22aa80d315980c732d7da3f51b167e11073d1b6
raghul-selsoft <raghul@selsoftinc.com>
2020-02-20 11:03:21 +0530

    dice filter issue

--- rts_angular/src/app/dice-intergration/dice-intergration.component.ts
+++ rts_angular/src/app/dice-intergration/dice-intergration.component.ts
@@ -248,7 +248,13 @@ export class DiceIntergrationComponent implements OnInit {
         var minExp: number;
         var maxExp: number;
         minExp = form.value.minExperience;
-        maxExp = form.value.maxExperience;
+        maxExp = form.value.maxExperience;       
+        if (form.value.minExperience === null) {
+            minExp = 0;
+        }
+        if (form.value.maxExperience === null) {
+            maxExp = 0;
+        }
         const submit = {
             userId: this.rtsUserId,
             q: this.selectedQuery,
